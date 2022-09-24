d1f2fdf9239f53f4569b562b2aff707388dbefe5
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-09-24 17:38:53 -0300

    mensajes de error

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -13,6 +14,7 @@ export class SignInComponent implements OnInit {
   username: string = '';
   password: string = '';
   confirmPassword: string = '';
+  loading: boolean = false;
 
   constructor(private toastr: ToastrService,
     private _userService: UserService,
@@ -24,13 +26,13 @@ export class SignInComponent implements OnInit {
   addUser() {
 
     // Validamos que el usuario ingrese valores
-    if(this.username == '' || this.password == '' || this.confirmPassword == '') {
+    if (this.username == '' || this.password == '' || this.confirmPassword == '') {
       this.toastr.error('Todos los campos son obligatorios', 'Error');
       return;
-    } 
+    }
 
     // Validamos que las password sean iguales
-    if(this.password != this.confirmPassword) {
+    if (this.password != this.confirmPassword) {
       this.toastr.error('Las passwords ingresadas son distintas', 'Error');
       return;
     }
@@ -41,12 +43,25 @@ export class SignInComponent implements OnInit {
       password: this.password
     }
 
-    this._userService.signIn(user).subscribe(data => {
-      this.toastr.success(`El usuario ${this.username} fue registrado con exito`, 'Usuario registrado');
-      this.router.navigate(['/login']);
+    this.loading = true;
+    this._userService.signIn(user).subscribe({
+      next: (v) => {
+        this.loading = false;
+        this.toastr.success(`El usuario ${this.username} fue registrado con exito`, 'Usuario registrado');
+        this.router.navigate(['/login']);
+      },
+      error: (e: HttpErrorResponse) => {
+        this.loading = false;
+        this.msjError(e);
+      }
     })
-
-    
   }
 
+  msjError(e: HttpErrorResponse) {
+    if (e.error.msg) {
+      this.toastr.error(e.error.msg, 'Error');
+    } else {
+      this.toastr.error('Upps ocurrio un error, comuniquese con el administrador', 'Error');
+    }
+  }
 }
